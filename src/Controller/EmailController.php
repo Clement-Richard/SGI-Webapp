657a1d875d554ea6b423d3de816fe0cf750b87c0
Clement-Richard <54627176+SkyDroxy@users.noreply.github.com>
2022-04-18 16:37:04 +0200

    Updated EmailController.php

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -4,15 +4,11 @@ namespace App\Controller;
 
 use Exception as GlobalException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mailer\Exception\ExceptionInterface;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-use Symfony\Component\Mime\Email;
 
 class EmailController extends AbstractController
 {
@@ -22,24 +18,34 @@ class EmailController extends AbstractController
         $data = json_decode(file_get_contents('php://input'), true);
         $mail = new PHPMailer(true);
         $recipient = '';
-        switch($data['formation']){
+        $nom = $data['name'];
+        $prenom = $data['firstname'];
+        $adresse = $data['address'];
+        $formation = $data['formation'];
+        $formationLabel = '';
+        switch($formation){
             case 1:
                 $recipient = 'informatique@sgi-groupe.fr';
+                $formationLabel = 'Informatique';
                 break;
             case 2:
                 $recipient = 'extranet@sgi-groupe.fr';
+                $formationLabel = 'Développement';
                 break;
             case 3:
                 $recipient = 'christian.sorace@sgi-groupe.fr';
+                $formationLabel = 'Systèmes et réseaux';
                 break;
             case 4:
                 $recipient = 'florian.bouchez@sgi-groupe.fr';
+                $formationLabel = 'Autre';
                 break;
         }
         
         try {
             //Server settings
             $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+            $mail->CharSet = 'UTF-8';
             $mail->isSMTP();
             $mail->Host       = $_ENV['PHPMAILER_HOST'];
             $mail->SMTPAuth   = true;
@@ -54,9 +60,8 @@ class EmailController extends AbstractController
 
             //Content
             $mail->isHTML(true);
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->Subject = 'Formation';
+            $mail->Body    = "<p>Nom: $nom<br>Prénom: $prenom<br>Adresse: $adresse<br>Formation: $formationLabel</p>";
 
             $mail->send();
             return new Response(
